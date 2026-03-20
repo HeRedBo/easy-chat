b65ab02576cf5e545ef58bd0a1e0533ac97c81b8
hehongbo <hhbjkd@163.com>
2026-03-20 12:44:00 +0800

    添加im服务air代码更新脚本与服务启动脚本

--- apps/im/ws/im.go
+++ apps/im/ws/im.go
@@ -3,12 +3,16 @@ package main
 import (
 	"flag"
 	"fmt"
+	"os"
+	"os/signal"
+	"syscall"
 
 	"github.com/HeRedBo/easy-chat/apps/im/ws/internal/config"
 	"github.com/HeRedBo/easy-chat/apps/im/ws/internal/handler"
 	"github.com/HeRedBo/easy-chat/apps/im/ws/internal/svc"
 	"github.com/HeRedBo/easy-chat/apps/im/ws/websocket"
 	"github.com/zeromicro/go-zero/core/conf"
+	"github.com/zeromicro/go-zero/core/logx"
 )
 
 var configFile = flag.String("f", "etc/local/im.yaml", "the config file")
@@ -28,5 +32,14 @@ func main() {
 	handler.RegisterHandlers(srv, ctx)
 
 	fmt.Printf("Starting rpc server at %s...\n", c.ListenOn)
+	// 3. 监听退出信号（SIGINT/SIGTERM），实现优雅关闭
+	quit := make(chan os.Signal, 1)
+	signal.Notify(quit, syscall.SIGINT, syscall.SIGTERM, syscall.SIGQUIT)
+	go func() {
+		<-quit
+		logx.Info("Received exit signal, shutting down server...")
+		srv.Stop() // 主动关闭服务器，释放端口
+		os.Exit(0)
+	}()
 	srv.Start()
 }
