2d5b24a2d348849654188bea2be822763d25104e
hehongbo <hhbjkd@163.com>
2026-03-24 17:49:03 +0800

    新增 任务模块热启动脚本 实现开发热启动

--- apps/task/mq/task.go
+++ apps/task/mq/task.go
@@ -3,11 +3,15 @@ package main
 import (
 	"flag"
 	"fmt"
+	"os"
+	"os/signal"
+	"syscall"
 
 	"github.com/HeRedBo/easy-chat/apps/task/mq/internal/config"
 	"github.com/HeRedBo/easy-chat/apps/task/mq/internal/handler"
 	"github.com/HeRedBo/easy-chat/apps/task/mq/internal/svc"
 	"github.com/zeromicro/go-zero/core/conf"
+	"github.com/zeromicro/go-zero/core/logx"
 	"github.com/zeromicro/go-zero/core/service"
 )
 
@@ -32,6 +36,14 @@ func main() {
 		serviceGroup.Add(s)
 	}
 	fmt.Printf("Starting mqueue server")
-
+	// 3. 监听退出信号（SIGINT/SIGTERM），实现优雅关闭
+	quit := make(chan os.Signal, 1)
+	signal.Notify(quit, syscall.SIGINT, syscall.SIGTERM, syscall.SIGQUIT)
+	go func() {
+		<-quit
+		logx.Info("Received exit signal, shutting down server...")
+		serviceGroup.Stop() // 主动关闭服务器，释放端口
+		os.Exit(0)
+	}()
 	serviceGroup.Start()
 }
---
+++ scripts/start-task.sh
@@ -0,0 +1,6 @@
+@echo off
+set "ROOT=%~dp0.."
+set "TARGET_DIR=%ROOT%\apps\task\mq"
+
+:: 新开 cmd 窗口
+start "im-ws" cmd /k "cd /d %TARGET_DIR% && air"
